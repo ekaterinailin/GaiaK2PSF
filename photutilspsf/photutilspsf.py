3c3514c3da52adf186ba9bd161798825ccd4d4bb
Ekaterina Ilin <ekaterina.ilin@posteo.de>
2019-04-25 09:54:06 +0200

    functions with fixed centroids in photutilspsf, also generated a FLC from results.

--- photutilspsf/photutilspsf.py
+++ photutilspsf/photutilspsf.py
@@ -19,6 +19,73 @@ from astropy.stats import gaussian_sigma_to_fwhm
 
 from lightkurve import KeplerTargetPixelFile
 
+def do_photometry_with_fixed_centroids(photometry, memberspath, TPF, index, **kwargs):
+    '''
+    Do photometry with fixed centroids.
+    
+    Parameters:
+    ------------
+    
+    kwargs : dict
+        Keyword arguments to  pass to 
+        wrapper_supplement_olivares_with_gaia_coordinates_on_tpf
+    '''
+    image = TPF.flux[index,:,:]
+    rselect = wrapper_supplement_olivares_with_gaia_coordinates_on_tpf(TPF,
+                                                                       memberspath,
+                                                                       **kwargs)
+
+    pos = Table(names=['EPIC','x_0', 'y_0'], data=[rselect['EPIC'],
+                                                   rselect['xcentroid'],
+                                                   rselect['ycentroid']])
+    result_table =  photometry(image=image, init_guesses=pos)
+    del image
+    return result_table
+
+def define_basic_photometry_with_fixed_centroids(sigma_psf=0.5, group_psf=1.5,
+                                                 fitshape=(11,11), psf_model=None):
+    '''
+    Define photometry with centroid given by members and Gaia
+    centroids.
+    
+    Parameters:
+    ------------
+    sigma_psf : float
+        1 sigma size in pixel
+    group_psf : float
+        maximum distance between stars for grouping
+    fitshape : tuple of float
+        Rectangular shape around the center of a star
+        which will be used to collect the data to do
+        the fitting. 
+    psf_model :  astropy.modeling.Fittable2DModel instance
+        PSF or PRF model to fit the data. Could be one of 
+        the models in this package like DiscretePRF, 
+        IntegratedGaussianPRF, or any other suitable 
+        2D model.
+    Returns:
+    ---------
+    photutils.psf.BasicPSFPhotometry object
+    '''
+    daogroup = DAOGroup(group_psf *
+                        sigma_psf * 
+                        gaussian_sigma_to_fwhm)
+
+    bkg = MMMBackground()
+    fitter = LevMarLSQFitter()
+    if psf_model is None:
+        psf_model = IntegratedGaussianPRF(sigma=sigma_psf)
+
+    psf_model.x_0.fixed = True
+    psf_model.y_0.fixed = True
+    
+    return BasicPSFPhotometry(group_maker=daogroup,
+                              bkg_estimator=bkg,
+                              psf_model=psf_model,
+                              fitter=LevMarLSQFitter(),
+                              fitshape=fitshape,
+                              aperture_radius=5)
+
 def define_iterative_photometry_with_finder(image, sigma_psf=0.5, group_psf=1.5,
                                             grouper=None,bkg=MMMBackground(),
                                             fitter=LevMarLSQFitter(),psf_model=None,
@@ -50,7 +117,7 @@ def define_iterative_photometry_with_finder(image, sigma_psf=0.5, group_psf=1.5,
         finder = IRAFStarFinder(threshold=3*std,
                           fwhm=2*sigma_psf*gaussian_sigma_to_fwhm,
                           minsep_fwhm=0.01, roundhi=5.0, roundlo=-5.0,
-                          sharplo=0.0, sharphi=8., brightest=50)
+                          sharplo=0.0, sharphi=8., brightest=10)
     
     return IterativelySubtractedPSFPhotometry(finder=finder,
                                                 group_maker=grouper,
@@ -61,7 +128,7 @@ def define_iterative_photometry_with_finder(image, sigma_psf=0.5, group_psf=1.5,
                                                 fitshape=fitshape,
                                                 aperture_radius=3)
 
-def wrapper_supplement_olivares_with_gaia_coordinates_on_tpf(TPF, path):
+def wrapper_supplement_olivares_with_gaia_coordinates_on_tpf(TPF, path, get_saved=False, brightest=1.):
     '''
     Supplement olivares table with gaia coordinates.
     Save the intermediate resulst
@@ -72,25 +139,34 @@ def wrapper_supplement_olivares_with_gaia_coordinates_on_tpf(TPF, path):
     
     path : str
         path to Olivares members table
+    get_saved : bool
+        if True will fetch a stored table
      
     Return:
     -------
     DataFrame with Olivares members and Gaia solution
-    '''
-    members = read_fits_table(path)
-    center, radius = find_center_and_radius(TPF)
-    others = get_gaia_astrometry(center, radius)
-    ontpf = merge_olivares_members_with_gaia_coordinates(members, others)
-    res = select_members_on_tpf(TPF, ontpf)
+    ''' 
     EPIC = TPF.targetid
     C = TPF.campaign
-    #save intermediate to file
-    with open('data/{}_{:02d}.csv'.format(EPIC, C), 'w') as f:
-        f.write('# This is a concatenated table of Olivares+2019\n'
-                '# and Gaia DR2 coordinates on the TargetPixelFile\n'
-                '# of EPIC {} in campaign {}\n#\n'.format(EPIC, C))
-        res.to_csv(f,index=False)
-    return res
+    datapath = 'data/{}_{:02d}.csv'.format(EPIC, C)
+    if get_saved==True:
+        return pd.read_csv(datapath, skiprows=4)
+    elif get_saved==False:
+        members = read_fits_table(path)
+        center, radius = find_center_and_radius(TPF)
+        others = get_gaia_astrometry(center, radius)
+        bright_part = (others.sort_values(by='phot_g_mean_flux')
+                             .iloc[:int(np.rint(brightest * others.shape[0])),:])
+        ontpf = merge_olivares_members_with_gaia_coordinates(members, bright_part)
+        res = select_members_on_tpf(TPF, ontpf)
+       
+        #save intermediate to file
+        with open(datapath, 'w') as f:
+            f.write('# This is a concatenated table of Olivares+2019\n'
+                    '# and Gaia DR2 coordinates on the TargetPixelFile\n'
+                    '# of EPIC {} in campaign {}\n#\n'.format(EPIC, C))
+            res.to_csv(f,index=False)
+        return res
 
 def merge_olivares_members_with_gaia_coordinates(members, gaia):
     '''
